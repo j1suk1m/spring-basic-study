e9f6a6e586382b01df788b731063c8da0c64c781
jisukimtech <jisukim.tech@gmail.com>
2025-01-13 15:20:07 +0900

    feat: [section 8] 애노테이션 직접 만들기

--- spring/src/main/java/study/spring/discount/RateDiscountPolicy.java
+++ spring/src/main/java/study/spring/discount/RateDiscountPolicy.java
@@ -3,11 +3,13 @@ package study.spring.discount;
 import lombok.Getter;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
+import study.spring.annotation.MainDiscountPolicy;
 import study.spring.member.Grade;
 import study.spring.member.Member;
 
 @Component
 @Primary
+@MainDiscountPolicy
 @Getter
 public class RateDiscountPolicy implements DiscountPolicy {
     private final int discountPercent = 10;
--- spring/src/main/java/study/spring/order/OrderServiceImpl.java
+++ spring/src/main/java/study/spring/order/OrderServiceImpl.java
@@ -2,18 +2,24 @@ package study.spring.order;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
+import study.spring.annotation.MainDiscountPolicy;
 import study.spring.discount.DiscountPolicy;
 import study.spring.member.Member;
 import study.spring.member.MemberRepository;
 
 @Component
-@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
     @Getter
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
+
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
