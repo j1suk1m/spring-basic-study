e5ef6fb65116ef8cad569dc3123b20a6cf4c787c
jisukimtech <jisukim.tech@gmail.com>
2025-01-14 12:24:30 +0900

    feat: [section 9] 애노테이션 `@`PostConstruct, `@`PreDestroy

--- spring/src/test/java/study/spring/lifecycle/NetworkClient.java
+++ spring/src/test/java/study/spring/lifecycle/NetworkClient.java
@@ -1,5 +1,8 @@
 package study.spring.lifecycle;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 public class NetworkClient {
     private String url;
 
@@ -7,12 +10,14 @@ public class NetworkClient {
         System.out.println("생성자 호출, url: " + url);
     }
 
+    @PostConstruct
     public void init() {
         System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메세지");
     }
 
+    @PreDestroy
     public void close() {
         System.out.println("NetworkClient.close");
         disconnect();
