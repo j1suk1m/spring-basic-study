aab39609fc1066879f31753c7bcf4d185bb72c73
jisukimtech <jisukim.tech@gmail.com>
2025-01-14 11:02:52 +0900

    feat: [section 9] 인터페이스 InitializingBean, DisposableBean

--- spring/src/test/java/study/spring/lifecycle/NetworkClient.java
+++ spring/src/test/java/study/spring/lifecycle/NetworkClient.java
@@ -1,12 +1,26 @@
 package study.spring.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
     private String url;
 
     public NetworkClient() {
         System.out.println("생성자 호출, url: " + url);
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("NetworkClient.afterPropertiesSet");
         connect();
         call("초기화 연결 메세지");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("NetworkClient.destroy");
+        disconnect();
     }
 
     public void setUrl(String url) {
