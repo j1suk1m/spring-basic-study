866f149fa664e0affc2db15e41e2440ad378952c
jisukimtech <jisukim.tech@gmail.com>
2025-01-15 15:29:47 +0900

    feat: [section 10] 스코프와 Provider

--- spring/src/main/java/study/spring/common/LogController.java
+++ spring/src/main/java/study/spring/common/LogController.java
@@ -2,6 +2,7 @@ package study.spring.common;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -10,11 +11,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequiredArgsConstructor
 public class LogController {
     private final LogService logService;
-    private final Logger logger;
+    private final ObjectProvider<Logger> loggerProvider;
 
     @RequestMapping("log")
     @ResponseBody
     public String log(HttpServletRequest request) {
+        Logger logger = loggerProvider.getObject();
         String requestURL = request.getRequestURL().toString();
         logger.setRequestURL(requestURL);
 
--- spring/src/main/java/study/spring/common/LogService.java
+++ spring/src/main/java/study/spring/common/LogService.java
@@ -1,14 +1,16 @@
 package study.spring.common;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class LogService {
-    private final Logger logger;
+    private final ObjectProvider<Logger> loggerProvider;
 
     public void executeLogic(String id) {
+        Logger logger = loggerProvider.getObject();
         logger.log("service id = " + id);
     }
 }
